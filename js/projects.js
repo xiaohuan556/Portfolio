7c3ab91d232b799ec3de3d587f24dfc8f60af578
Jason <huan.guo@kikatech.com>
2026-03-11 10:35:02 +0800

    site_update_周三 2026/03/11_10:35:02.68

--- js/projects.js
+++ js/projects.js
@@ -1,28 +1,29 @@
 const projectData = {
   "works": [
     {
-      "videoUrl": "assets/videos/01__works__%E4%BD%9C%E5%93%81%E5%90%8D__%E5%9B%A0%E4%B8%BA%E5%95%8A%E6%98%AF%E6%BB%B4%E6%92%92%E6%89%93%E6%AD%BB%E5%AE%89%E9%9D%99%E5%9C%B0%E6%92%92%E5%86%B3%E8%B5%9B%E5%A4%A7%E7%A5%AD%E5%8F%B8%E5%95%8A%E7%9A%84.mp4",
-      "title": "作品名",
-      "desc": "因为啊是滴撒打死安静地撒决赛大祭司啊的"
+      "videoUrl": "assets/videos/01__works__%E5%88%98%E7%8E%89%E6%96%B0%E5%A9%9A%E5%BF%AB%E4%B9%90__2026%E6%96%B0%E5%A9%9A%E5%A4%A7%E5%90%89.mp4",
+      "title": "刘玉新婚快乐",
+      "desc": "2026新婚大吉"
     }
   ],
   "cinematic": [
     {
-      "videoUrl": "assets/videos/01__cinematic__%E6%A0%87%E9%A2%98%E8%BF%99%E6%98%AF__2026%E6%98%A5%E5%AD%A3%E8%A7%86%E8%A7%89%E5%8D%87%E7%BA%A7%E6%96%B9%E6%A1%88.mp4",
-      "title": "标题这是",
-      "desc": "2026春季视觉升级方案"
+      "videoUrl": "assets/videos/01__cinematic__%E5%88%98%E7%8E%89%E6%96%B0%E5%A9%9A%E5%BF%AB%E4%B9%90__2026%E6%96%B0%E5%A9%9A%E5%A4%A7%E5%90%89.mp4",
+      "title": "刘玉新婚快乐",
+      "desc": "2026新婚大吉"
     }
   ],
-  "commercial": [
+  "commercial": [],
+  "commercial_刘玉新婚快乐": [
     {
-      "videoUrl": "assets/videos/01__commercial__%E4%BD%9C%E5%93%81%E5%90%8D__%E9%98%BF%E8%90%A8.mp4",
-      "title": "作品名",
-      "desc": "阿萨"
+      "videoUrl": "assets/videos/01__commercial_%E5%88%98%E7%8E%89%E6%96%B0%E5%A9%9A%E5%BF%AB%E4%B9%90__2026%E6%96%B0%E5%A9%9A%E5%A4%A7%E5%90%89%20(2).mp4",
+      "title": "2026新婚大吉 (2).mp4",
+      "desc": ""
     },
     {
-      "videoUrl": "assets/videos/01__commercial__%E4%BD%9C%E5%93%81%E5%90%8D__%E5%95%8A%E6%98%AF%E5%A4%9A%E5%B0%91%E6%89%93%E6%92%92%E7%9A%84.mp4",
-      "title": "作品名",
-      "desc": "啊是多少打撒的"
+      "videoUrl": "assets/videos/01__commercial_%E5%88%98%E7%8E%89%E6%96%B0%E5%A9%9A%E5%BF%AB%E4%B9%90__2026%E6%96%B0%E5%A9%9A%E5%A4%A7%E5%90%89.mp4",
+      "title": "2026新婚大吉.mp4",
+      "desc": ""
     }
   ]
 };
